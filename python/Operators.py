e3afa8af49b16a0cb54df2822434b200f32a404d
Anand <anandbanoth3755@gmail.com>
2025-07-23 23:46:57 +0530

    pyhton practice files

---
+++ python/Operators.py
@@ -0,0 +1,25 @@
+'''
+Task 3: Logical Operators
+a, b = map(int, input ("Enter two numbers: ").split())
+a = 20
+b = 15
+print ("a =", a , ",", "b =", b)
+print ("a > b", a>b)
+print ("a == b:", a==b)
+print ("a != b:", a!=b)
+#######
+a, b = map(int, input ("Enter two numbers: ").split())
+print (f"a = {a}, b = {b} \na > b: {a>b}\na = b: {a==b}\na != b: {a!=b}")'''
+####################################################################################
+'''Task 4: Assignment Operators
+x = 10
+x += 5
+print ("X+= 5:", x)
+x -= 3
+print ("X-= ", x)
+x *= 2
+print ("X *=", x)
+x /= 4
+print ("X /=", x)
+print(f"x += {x += 3}\nx -= ")'''
+#################################################################
---
+++ python/variable.py
@@ -0,0 +1,28 @@
+'''name = "Anand"
+age = 27
+country = "india"
+employed = True
+salary = 3.6
+
+print (f"name:{name}\nage: {age}\ncountry: {country}\nsalary: {salary}\nemployed: {employed}")'''
+#################################
+'''Swapping
+city1 = "hyd"
+city2 = "bnglr"
+city1,  city2 = city2, city1
+print(f"city1:{city2}, city2: {city1}")'''
+#################################
+'''printing numbers
+a = 3
+b = 5
+print (a + b)'''
+#################################
+'''###Input and Output (I/O)###
+name = input ("enter your name: ")
+age = int (input("your age: "))
+height = float(input("your height please: "))
+#name, age, height = age, height, name
+student = input("your a student or not? yes/no: ")
+student = student.lower() in ["yes", "y", "true", "1"]
+student_status = "yes" if student else "no"
+print(f"hello your name is:{name}\n your age is:{age} years\n height please:{height}\n student:{student}\n student-status:{student_status}")'''
