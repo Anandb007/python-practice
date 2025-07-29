8d79680294869e1922d35f936d290fa2a4d8dd13
Anand <anandbanoth3755@gmail.com>
2025-07-29 07:26:57 +0530

    task file updated

--- python/task.py
+++ python/task.py
@@ -25,3 +25,13 @@ status = status.lower() in ("yes", "y", "true", "5")
 status = "Yes" if status else "No"
 experience = float(input("if you have an experiece then how many year: "))
 print(f"project: {project}\nyear: {year}\nstatus: {status}\nexperience: {experience}")'''
+##################################################
+is_adult = input("Are you an adult? (yes/no): ")
+has_ticket = input("Do you have a ticket? (yes/no): ")
+
+is_adult_bool = is_adult.lower() in ("yes", "y", "true", "1")
+has_ticket_bool = has_ticket.lower() in ("yes", "y", "true", "1")
+print (f"AND result: {is_adult_bool and has_ticket_bool}")
+print (f"or result: {is_adult_bool or has_ticket_bool}")
+print (f"not result: { not has_ticket_bool}")
+print (f"not result: { not is_adult_bool}")
